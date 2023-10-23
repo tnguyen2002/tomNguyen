c00d6b0aedc43a53048fd18f655aa4b7a12fce77
tnguyen2002 <tnguyen65699@gmail.com>
2023-10-23 00:50:20 -0700

    update

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -2,55 +2,49 @@ import SocialIcons from "../SocialIcon/SocialIcon";
 import { NavLink } from "react-router-dom";
 import "./Home.css";
 function Home() {
-	return (
-		<div className="flex flex-col w-full sm:w-1/4">
-			<SocialIcons />
-			<div>
-				<div className="font-bold text-lg">Tom Nguyen</div>
-			</div>
-			<div className="Section">
-				<div>
-					<div>
-						I am interested in creating AI/ML products and currently working @{" "}
-						<a
-							className="font-bold text-rose-500"
-							href="https://www.nasa.gov/ames"
-						>
-							NASA
-						</a>
-						. I am currently in my 4th year @{" "}
-						<a
-							className="font-bold text-rose-500"
-							href="https://www.stanford.edu/"
-						>
-							Stanford
-						</a>{" "}
-						and will soon be pursuing a masters. In my free time I enjoy playing
-						volleyball and making videos on{" "}
-						<a
-							className="font-bold text-rose-500"
-							href="https://www.youtube.com/@tomnguyen4548"
-						>
-							Youtube
-						</a>
-						. My academic{" "}
-						<a
-							className="font-bold text-rose-500"
-							href="https://airtable.com/appNGte73TR0vBXG2/shrPevp160CKqhAKv"
-						>
-							background
-						</a>{" "}
-						is primarily computer science with specks of economics and math.
-					</div>
-					<div>Contact: anhn@stanford.edu</div>
-				</div>
-			</div>
-			<div className="font-bold text-rose-500">
-				<NavLink className="text-rose-500" to="/projects">
-					Projects →
-				</NavLink>
-			</div>
-		</div>
-	);
+  return (
+    <div className="flex flex-col w-full sm:w-1/4">
+      <SocialIcons />
+      <div>
+        <div className="font-bold text-lg">Tom Nguyen</div>
+      </div>
+      <div className="Section">
+        <div>
+          <div>
+            I am interested in creating AI/ML products. I am currently in my 4th
+            year @{" "}
+            <a
+              className="font-bold text-rose-500"
+              href="https://www.stanford.edu/"
+            >
+              Stanford
+            </a>{" "}
+            and will soon be pursuing a masters. In my free time I enjoy playing
+            volleyball and making videos on{" "}
+            <a
+              className="font-bold text-rose-500"
+              href="https://www.youtube.com/@tomnguyen4548"
+            >
+              Youtube
+            </a>
+            . My academic{" "}
+            <a
+              className="font-bold text-rose-500"
+              href="https://airtable.com/appNGte73TR0vBXG2/shrPevp160CKqhAKv"
+            >
+              background
+            </a>{" "}
+            is primarily computer science with specks of economics and math.
+          </div>
+          <div>Contact: anhn@stanford.edu</div>
+        </div>
+      </div>
+      <div className="font-bold text-rose-500">
+        <NavLink className="text-rose-500" to="/projects">
+          Projects →
+        </NavLink>
+      </div>
+    </div>
+  );
 }
 export default Home;
