b313a9a36025f46452910d21d13f27f4c24b7507
Anh (Tom) Nguyen <tnguyen65699@gmail.com>
2025-08-19 20:21:17 -0500

    fixed must haves style

--- src/components/DreamJobCriteria/DreamJobCriteria.tsx
+++ src/components/DreamJobCriteria/DreamJobCriteria.tsx
@@ -1,11 +1,11 @@
 function DreamJobCriteria() {
   const mustHaves = [
     {
-      text: "quirky mle along the brainwaves of pipelines for generating best dating profiles, insta captions, or converting irl movies to different anime styles",
+      text: "be an mle developing along the brainwaves of pipelines for generating best dating profiles, insta captions, or converting irl movies to different anime styles",
     },
     { text: "be a coding youtuber" },
     {
-      text: "company with a culture of 'a little bit of slope makes up for a lot of y-intercept' - john ousterhout",
+      text: "in a company with a culture of 'a little bit of slope makes up for a lot of y-intercept' - john ousterhout",
     },
   ];
 
@@ -13,8 +13,8 @@ function DreamJobCriteria() {
     <div className="flex flex-col w-full items-start text-left">
       <div className="text-xl mb-2">
         {mustHaves.map((item, idx) => (
-          <div key={idx} className="mb-1 sm:mb-2 flex items-center">
-            <span className="font-bold mr-2">{idx + 1}.</span>
+          <div key={idx} className="mb-1 sm:mb-2 flex items-start">
+            <span className="font-bold mr-2 pt-0.5">{idx + 1}.</span>
             <span>{item.text.toLowerCase()}</span>
           </div>
         ))}
